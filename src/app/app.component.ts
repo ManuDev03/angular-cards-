86150e785e2805b50c77bfd677102eec633abeb2
ManuDev03 <=manuprajan03@gmail.com>
2021-11-07 18:41:12 +0530

    parent to child communication accepting data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    {
+      title:'Neat tree',
+      imageUrl:"assets/tree.jpeg",
+      username:"nature",
+      content:"the best tree in my backyard"
+    },
+    {
+      title:'Mountain',
+      imageUrl:"assets/mountain.jpeg",
+      username:"mountainlover",
+      content:"the best view from this mountain"
+    },
+    {
+      title:'Biker',
+      imageUrl:"assets/biking.jpeg",
+      username:"bikelover",
+      content:"the best ride is in evening"
+    }
+  ]
 }
